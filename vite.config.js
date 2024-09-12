acdf1c8c5ea193f7fa6cd1d947216d62825c731a
Jun <jz7259@g.rit.edu>
2024-09-12 00:57:49 -0400

    testing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/JunAndFiona/",
+  base: "/fionaandjun/",
   plugins: [react()],
   css:{
     module: {
